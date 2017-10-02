bf561d4cee52f9c19509bc164010f5637baba578
trupovar13 <trupovar13@gmail.com>
2017-10-02 08:39:06 +0300

    Reafctoring: extract logger to separate file

--- app.rb
+++ app.rb
@@ -1,34 +1,8 @@
-class Logger
-  def initialize
-    @f = File.open 'log.txt', 'a'
-  end
+require './logger'
 
-  @@x = nil
 
-  def self.instance
-    if @@x == nil
-      @@x = Logger.new
-    end
+Logger.instance.log_something 'bla bla!!'
 
-    return @@x
-  end
 
-  # class method
-  def self.say_something
-    puts "haha"
-  end
 
-  # instance method
-  def log_something wat
-    @f.puts wat
-    end
-
-end
-
-Logger.say_something
-Logger.instance.log_something 'blabla'
-Logger.instance.log_something 'blabla2'
-
-logger = Logger.new
-logger.log_something 'hey!'
 
