efc4ce6b98df3346268039ac5739e305ac1b2a9e
trupovar13 <trupovar13@gmail.com>
2017-10-02 10:37:50 +0300

    Add to logger.rb

--- logger.rb
+++ logger.rb
@@ -1,20 +1,14 @@
+require 'singleton'
+
 class Logger
+  include Singleton
+
   def initialize
     @f = File.open 'log.txt', 'a'
   end
 
-  @@x = Logger.new
-
-  def self.instance
-    return @@x
-  end
-
-
-  # instance method
   def log_something wat
     @f.puts wat
-    end
-
-    private_class_method :new
-
+    @f.flush
+  end
 end
